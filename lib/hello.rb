9522a056efa1b3b64169538e56a4425bc8af79b9
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-19 13:55:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,19 +1,14 @@
 def hello_t(array)
-  if block_given?
+ # if block_given?
   i = 0
   while i < array.length
     yield array[i]
     i += 1
   end
   array
-  else
-    puts "Hey! No block was given!"
-  end
+#  else
+ #   puts "Hey! No block was given!"
+ # end
 end
 
-# call your method here!
-hello_t(["Ubaid", "Junaid", "Uzzam", "Aalyan"]) do |name|
-  if name.start_with?("U")
-    puts "Hi, #{name}"
-  end
-end
+
